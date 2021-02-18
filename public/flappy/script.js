af60360d28769cccd29426a8c8b5771a553af209
Vincent Tann <vtann17@gmail.com>
2021-02-18 17:14:50 -0500

    progression with flappy bird

---
+++ public/flappy/script.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const bird = document.querySelector('.bird')
+  const gameDisplay = document.querySelector('.game-container')
+  const ground = document.querySelector('.ground')
+  
+  let birdLeft = 220
+  let birdBottom = 100
+  let gravity = 2
+  
+  function startGame() {
+    /* birdBottom -= gravity */
+    bird.style.bottom = birdBottom + 'px'
+    bird.style.left = birdLeft + 'px'
+  }
+  let timerId = setInterval(startGame, 20)
+  
+  function control(e) {
+      if (e.keyCode === 32) {
+          jump()
+      }
+  }
+
+  function jump() {
+    if (birdBottom < 680) birdBottom += 50  
+    bird.style.bottom = birdBottom + 'px'
+    console.log(birdBottom)
+  }
+  document.addEventListener('keyup', jump)
+
+  function generateObstacle() {
+      let obstacleLeft = 500
+      let randomHeight = Math.random() * 60
+      let obstacleBottom = randomHeight
+      const obstacle = document.createElement('div')
+      obstacle.classList.add('obstacle')
+      gameDisplay.appendChild(obstacle)
+      obstacle.style.left = obstacleLeft + 'px'
+      obstacle.style.bottom = obstacleBottom + 'px'
+
+      function moveObstacle() {
+          obstacleLeft -= 2
+          obstacle.style.left = obstacleLeft + 'px'
+
+          if(obstacleLeft === -60) {
+              clearInterval(timerId)
+              gameDisplay.removeChild(obstacle)
+          }
+      }
+      let timerId = setInterval(moveObstacle, 20)
+      setTimeout(generateObstacle, 3000)
+  }
+  generateObstacle()
+})
