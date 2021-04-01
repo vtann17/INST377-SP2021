2baa01a84ba528b15d393c154a5d535f03e4cdc6
Vincent Tann <vtann17@gmail.com>
2021-04-01 00:03:34 -0400

    update

--- public/doodle/script.js
+++ public/doodle/script.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let isGoingRight = false
     let leftTimerId
     let rightTimerId
+    let score = 0
 
     function createDoodler() {
         grid.appendChild(doodler)
@@ -56,6 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     let firstPlatform = platforms[0].visual
                     firstPlatform.classList.remove('platform')
                     platforms.shift()
+                    score ++
                     console.log(platforms)
                     let newPlatform = new Platform(600)
                     platforms.push(newPlatform)
@@ -104,6 +106,10 @@ document.addEventListener('DOMContentLoaded', () => {
     function gameOver() {
         console.log('game over')
         isGameOver = true
+        while (grid.firstChild) {
+            grid.removeChild(grid.firstChild)
+        }
+        grid.innerHTML = score
         clearInterval(upTimerId)
         clearInterval(downTimerId)
         clearInterval(leftTimerId)
