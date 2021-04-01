abee426f56aef3d5e6d84cea845013be97a2b9a9
Vincent Tann <vtann17@gmail.com>
2021-03-31 23:51:59 -0400

    update

--- public/doodle/script.js
+++ public/doodle/script.js
@@ -51,6 +51,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 platform.bottom -= 4
                 let visual = platform.visual
                 visual.style.bottom = platform.bottom + 'px'
+
+                if (platform.bottom < 10 {
+                    let firstPlatform = platforms[0].visual
+                    firstPlatform.classList.remove('platform')
+                    platforms.shift()
+                    console.log(platforms)
+                })
             })
         }
     }
