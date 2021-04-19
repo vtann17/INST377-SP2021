a71c613c5c1645d9dd1ff9dc81f99ccef35cd732
Vincent Tann <vtann17@gmail.com>
2021-04-19 18:39:02 -0400

    lab 4 retry

--- server.js
+++ server.js
@@ -39,22 +39,14 @@ app.route('/api')
     console.log('GET request detected');
     const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
     const json = await data.json();
-    console.log('data from fetch', json[0]);
+    console.log('data from fetch', json);
     res.json(json);
   })
   .post(async (req, res) => {
     console.log('POST request detected');
-    console.log('Form data in res.body', req.body);
+    console.log('Form data in res.body', req.body());
     console.log('Now send something back to your client');
-    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
-    const json = await data.json();
-
-    fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
-      .then((data) => data.json())
-      .then((data2) => {
-        // do something with your data!
-      })
-      .catch((err) => console.error(err));
+    res.send(['Hello World'])
 
     res.json({data: json});
   });
